3a0d62c91756267bfad947f1448236988fed08e5
Kristóf Egressy <egressyk@gmail.com>
2017-11-23 12:52:44 +0100

    REFACTORING: replaced magical numbers in score function

--- bowling.py
+++ bowling.py
@@ -1,4 +1,6 @@
 def score(game):
+    MAX_SCORE = 10
+    MAX_FRAME = 10
     game = game.lower()
     result = 0
     frame = 1
@@ -6,17 +8,17 @@ def score(game):
 
     for i in range(len(game)):
         if game[i] == '/':
-            result += 10 - last
+            result += MAX_SCORE - last
         else:
             result += get_value(game[i])
 
-        if frame < 10 and get_value(game[i]) == 10:
+        if frame < MAX_FRAME and get_value(game[i]) == MAX_SCORE:
             if game[i] == '/':
                 result += get_value(game[i+1])
             elif game[i] == 'x':
                 result += get_value(game[i+1])
                 if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+                    result += MAX_SCORE - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
 
@@ -25,11 +27,12 @@ def score(game):
         if in_first_half is False:
             frame += 1
 
-        in_first_half = not in_first_half
-
         if game[i] == 'x':
             in_first_half = True
             frame += 1
+        else:
+            in_first_half = not in_first_half
+
 
     return result
 
